c1ac836e2192adfea9d60ef9cabd72278f0bac00
Dovskie <164857854+Dovskie@users.noreply.github.com>
2025-08-09 01:38:40 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1686,6 +1686,7 @@
 	for _, island in ipairs(tpFolder:GetChildren()) do
 		if island:IsA("BasePart") then
 			local btn = Instance.new("TextButton")
+			btn.Name = island.Name
 			btn.Size = UDim2.new(1, 0, 0.1, 0)
 			btn.Position = UDim2.new(0, 0, (0.1 + 0.02) * index, 0)
 			btn.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
